f3457fffd5370716e9b7c9d2a8220552a122bbb3
Tot <monsa0514@gmail.com>
2026-05-19 16:56:34 -0500

    Cuarto y quinto incremento: Añadir hora de tareas y sistema de notificaciones respectivamente

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const nombreTarea = document.getElementById('nombre-tarea');
+const horaTarea = document.getElementById('hora-tarea');
 const botónAgregarTarea = document.getElementById('botón-agregar-tarea');
 const listaTareas = document.getElementById('lista-tareas');
 
@@ -6,12 +7,18 @@ let tareas = JSON.parse(localStorage.getItem('misTareas')) || [];
 
 botónAgregarTarea.addEventListener('click', function() {
     const textoDeLaTarea = nombreTarea.value;
+    const valorHoraTarea = horaTarea.value;
 
     if (textoDeLaTarea !== ""){
 
-        tareas.push({ texto: textoDeLaTarea });
+        tareas.push({ 
+            texto: textoDeLaTarea,
+            hora: valorHoraTarea,
+            notificada: false,
+        });
         renderizarTareas();
         nombreTarea.value = "";
+        horaTarea.value = "";
     }
 });
 
@@ -20,7 +27,7 @@ function renderizarTareas(){
 
     tareas.forEach(function(tarea, indice){
         const nuevaTarea = document.createElement('li');
-        nuevaTarea.textContent=tarea.texto + " ";
+        nuevaTarea.textContent=tarea.texto + " - " + (tarea.hora || "Sin hora") + " ";
 
         const botonEliminar = document.createElement('button');
         botonEliminar.textContent = "Eliminar";
@@ -37,4 +44,16 @@ function renderizarTareas(){
     localStorage.setItem('misTareas', JSON.stringify(tareas));
 }
 
+    setInterval(function(){
+        const ahora = new Date();
+        const horaActual = ahora.getHours().toString().padStart(2, '0') + ":" + ahora.getMinutes().toString().padStart(2, '0');
+        tareas.forEach(function(tarea){
+            if(tarea.hora === horaActual && !tarea.notificada){
+                alert("⏰ ¡Es hora de tu tarea!: " + tarea.texto);
+
+                tarea.notificada = true;
+                renderizarTareas()
+            }
+        });
+    },1000);
 renderizarTareas();
